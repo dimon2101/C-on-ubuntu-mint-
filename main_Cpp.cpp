796531dc3f790f119bef069d81d5afc0970e821c
dimon2101 <36691530+dimon2101@users.noreply.github.com>
2019-10-20 20:05:09 +0500

    Create main_Cpp.cpp

---
+++ main_Cpp.cpp
@@ -0,0 +1,20 @@
+//dont works with ubuntu (LM-19.2) at beginning
+
+/*
+Checking for existence: /home/sara/Pulpit/kurs c/test1/bin/Debug/test1
+Executing: xterm -T test1 -e /usr/bin/cb_console_runner LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. /home/sara/Pulpit/kurs\ c/test1/bin/Debug/test1  (in /home/sara/Pulpit/kurs c/test1/.)
+Process terminated with status -1 (0 minute(s), 0 second(s))
+*/
+//sudo apt-get install g++
+//sudo apt-get install xterm
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
